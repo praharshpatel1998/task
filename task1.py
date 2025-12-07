1d1dd3ed5a3a6b0606f131a9e2d8dc3268a21544
praharshpatel1998 <praharsh.patel.98@gmail.com>
2025-12-07 13:59:35 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,15 @@
+
+# task 1 addition / subtraction / multiplication /division
+num1 = int(input("Enter a number"))
+num2 = int (input("Enter a number"))
+
+
+addition  = num1 + num2
+Subtraction = num1 - num2
+multiplication = num1 * num2
+division = float(num1) / float(num2)
+
+print(addition)
+print(Subtraction)
+print(multiplication)
+print(division)
