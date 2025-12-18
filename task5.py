6d5ea647f0b2365253e7ff9f484a93c0f67c247b
praharshpatel1998 <praharsh.patel.98@gmail.com>
2025-12-18 23:43:14 +0530

    Add files via upload
    task 5 and 6

---
+++ task5.py
@@ -0,0 +1,9 @@
+def factorial(num):
+    if num == 1:
+        return 1
+    else :
+        fact= num * factorial(num - 1)
+        return fact
+
+num = int(input("Enter the number "))
+print(f"factorial of {num} is  {factorial(num)} : ")
---
+++ task6.py
@@ -0,0 +1,9 @@
+import math
+x = float(input("Enter the number "))
+x1 = math.sqrt(x)
+x2 = math.log(x)
+x3 = math.sin(x)
+
+print(f"square root :{x1}")
+print(f"Logarithm :{x2}")
+print(f"Sine :{x3}")
